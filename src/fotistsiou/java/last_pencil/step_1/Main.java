d70c0ed65d8d4f5b689e8cd7df89d82988a48c31
fotistsiou <fotis.tsioumas@outlook.com>
2024-11-29 19:29:57 +0200

    Update steps

---
+++ src/fotistsiou/java/last_pencil/step_1/Main.java
@@ -0,0 +1,17 @@
+package fotistsiou.java.last_pencil.step_1;
+
+/**
+ * Empty the pencil case
+ * ---------------------
+ * Description
+ * You and your friend decided to play a simple pen-and-paper game. There's one catch — you have only pencils but no
+ * paper; the last piece of paper is gone for your beautiful drawings. As a joke, your friend pulls all the pencils
+ * out of the case onto the table and says: "Your turn!"
+ * ---------------------
+ * Objectives
+ * In this stage, your program should print two lines: one line with several vertical bar symbols representing pencils
+ * (for example, ||| or |||||||) and one Your turn! string.
+ */
+
+public class Main {
+}
---
+++ src/fotistsiou/java/last_pencil/step_3/Main.java
@@ -0,0 +1,18 @@
+package fotistsiou.java.last_pencil.step_3;
+
+/**
+ * Working on the gameplay
+ * -----------------------
+ * Description
+ * An interesting idea has come to your mind. Let's change the rules of game. Players take turns taking X pencils until
+ * none of them remain.
+ * -----------------------
+ * Objectives
+ * Expand your program by creating a loop. Each player takes turns removing pencils until 0 pencils remain on the table.
+ * Each iteration prints 2 lines: lines with pencils (vertical bars) and whose turn is now by printing the *NameX*'s
+ * turn string. If there are no pencils left after a player's move, the loop breaks and the game ends without any
+ * additional output.
+ */
+
+public class Main {
+}
